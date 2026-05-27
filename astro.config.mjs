baa775fb6bec5cba3b68d4df0dd06e37f62ec675
dudnikov-a <alex.dudnikov@gmail.com>
2026-05-27 23:47:25 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [tailwind(), sitemap()],
+  site: 'https://remont-dvig123.pages.dev',
+  integrations: [tailwind()],
   output: 'static',
+  trailingSlash: 'always',
 });
